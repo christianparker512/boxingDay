aa11fc526a84f24db048a426b3bb0216791a2a38
Christian Parker <christianparker512@gmail.com>
2021-09-13 08:53:40 -0500

    with getters and setters

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -2,6 +2,20 @@ package parker.learningJava;
 
 import java.util.ArrayList;
 
+class IntClass{
+    private int myValue;
+
+    public intClass(int myValue){
+        this.myValue = myValue;
+    }
+    public int getMyValue(){
+        return myValue;
+    }
+    public void setMyValue(int myValue){
+        this.myValue = myValue;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
