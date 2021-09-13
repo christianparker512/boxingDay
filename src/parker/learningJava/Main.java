4db2771f381922581e7c2e0a0dff212e7f1d45ae
Christian Parker <christianparker512@gmail.com>
2021-09-13 08:51:08 -0500

    string array list

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -1,8 +1,15 @@
 package parker.learningJava;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	
+	    String[] strArray = new String[10];
+	    int [] intArray = new int [10];
+
+        ArrayList<String> strArrayList = new ArrayList<String>();
+        strArrayList.add("Christian");
+
     }
 }
